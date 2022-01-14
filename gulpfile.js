e81671b5a720cae37e25b9209ce50120bb0901e0
pianist196 <1234569050@rambler.ru>
2022-01-14 22:21:07 +0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+import gulp from 'gulp'
+import { path } from './gulp/config/path.js'
+import { plugins } from "./gulp/config/plugins.js"
+
+global.app = {
+    path: path,
+    gulp: gulp,
+    plugins: plugins
+}
+
+import { copy } from './gulp/tasks/copy.js'
+import { reset } from "./gulp/tasks/reset.js"
+import { html } from "./gulp/tasks/html.js"
+import { server } from "./gulp/tasks/server"
+
+function watcher() {
+    gulp.watch(path.watch.files, copy)
+    gulp.watch(path.watch.html, html)
+}
+
+const mainTasks = gulp.parallel(copy, html)
+
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server))
+
+gulp.task('default', dev)
